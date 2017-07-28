29768d3141d144278aa67754e36ce623eba0ec9a
kishor1221 <kishor.nrpt@gmail.com>
2017-07-28 23:54:35 +0530

    test

---
+++ SacnnerTest.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+public class SacnnerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		for(int i=0; i<sc.nextInt(); i++)
+		{
+//		System.out.println(sc.next());
+		}
+
+	}
+
+}
